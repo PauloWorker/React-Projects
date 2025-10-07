9c47299a7b5fe048fa5ebe0362c5dd2bb1969a95
PauloWorker <pauloribierotrabalho@gmail.com>
2025-10-07 15:54:27 -0300

    Updated the listGroup.tsx to show which list theperson clicked

--- my-react-app/src/components/listGroup.tsx
+++ my-react-app/src/components/listGroup.tsx
@@ -1,4 +1,5 @@
 import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
 
@@ -11,14 +12,15 @@ function ListGroup() {
         'Veneza'
     ];
     // items = [];
-    let selectedIndex = -1;
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     const message = () => {
         return items.length === 0 && <p>No items found</p>;
     }
 
-    const handleClick = (event: MouseEvent) => {
-        console.log(event);
+    const handleClick = (index: number) => {
+        console.log(index);
+        setSelectedIndex(index);
     }
 
     return (
@@ -26,7 +28,7 @@ function ListGroup() {
             <ul className="list-group">
                 {message()}
                 {items.map((item, index) => ( 
-                    <li onClick={() => selectedIndex = index} key={item} className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}>{item}</li>
+                    <li onClick={() => handleClick(index)} key={item} className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}>{item}</li>
                 ))}
             </ul>
         </>
