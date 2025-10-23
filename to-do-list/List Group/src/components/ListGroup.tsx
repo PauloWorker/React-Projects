f9c414d5c9afde4e63b202cb03d3de89243d9343
PauloWorker <pauloribierotrabalho@gmail.com>
2025-10-23 13:16:05 -0300

    feat: implement input form for adding to-do items and enhance ListGroup component functionality

--- to-do-list/List Group/src/components/ListGroup.tsx
+++ to-do-list/List Group/src/components/ListGroup.tsx
@@ -1,5 +1,38 @@
-function ListGroup(){
-    return(
+import { useState } from "react";
+
+function ListGroup() {
+
+    const [lists, setLists] = useState([""]);  // <-- state array
+    const [name, setName] = useState("");
+
+    const add_button = (event: any) => {
+        setName(event.target.value);
+    }
+
+    const confirmName = (event: any) => {
+        event.preventDefault();
+
+        // Add item SAFELY (don't mutate state)
+        setLists([...lists, name.trim()]);
+        
+        // clear input
+        setName("");
+    }
+
+    const list = lists.map((item, index) => {
+        return item.length > 0 && (
+            <div key={index} className="list border border-primary rounded-1 m-1">
+                {item}
+                <div className="options">
+                    <button className="btn btn-outline-success btn-sm">finish</button>
+                    <button className="btn btn-outline-warning btn-sm">edit</button>
+                    <button className="btn btn-outline-danger btn-sm">delete</button>
+                </div>
+            </div>
+        );
+    });
+
+    return (
         <>
             {/* Title */}
             <div>
@@ -8,16 +41,24 @@ function ListGroup(){
 
             {/* Type bar */}
             <div className="input-group mb-3">
-                <input className="form-control" type="text" name="type-bar" id="type-bar" placeholder="Type here what you want to add" />
-                <span className="input-group-text btn btn-success col-sm-1" id="add-button">Add</span>
+                <form onSubmit={confirmName} className="input-group">
+                    <input
+                        className="form-control border"
+                        type="text"
+                        value={name}
+                        onChange={add_button}
+                        placeholder="Type here what you want to add"
+                    />
+                    <button className="input-group-text btn btn-success col-sm-1">
+                        Add
+                    </button>
+                </form>
             </div>
 
             {/* Lists here */}
             <div className="lists-window">
                 <div className="row">
-                    <div className="list border border-primary rounded-1">
-                        TodoList
-                    </div>
+                    {list}
                 </div>
             </div>
         </>
